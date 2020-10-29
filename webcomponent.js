52ed8172fd43c6a24679ba33417381031a079ebd
evgeny.grin@iprosis.com <evgeny.grin@iprosis.com>
2020-10-29 10:49:43 +0200

    updates

--- webcomponent.js
+++ webcomponent.js
@@ -1,14 +1,14 @@
 (function () {
 
-	let jQuieryScript = document.createElement('script');
-    jQuieryScript.src = 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js';
-    jQuieryScript.async = false;
-	document.head.appendChild(jQuieryScript);
+	// let jQuieryScript = document.createElement('script');
+    // jQuieryScript.src = 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js';
+    // jQuieryScript.async = false;
+	// document.head.appendChild(jQuieryScript);
 
-	// let popperScript = document.createElement('script');
-    // popperScript.src = 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js';
-    // popperScript.async = false;
-	// document.head.appendChild(popperScript);
+	let popperScript = document.createElement('script');
+    popperScript.src = 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js';
+    popperScript.async = false;
+	document.head.appendChild(popperScript);
 
 	let bootstrapScript = document.createElement('script');
     bootstrapScript.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js';
