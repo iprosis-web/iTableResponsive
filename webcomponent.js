13f6ab96b02cd7836f898fe706e718b6eea94a59
evgeny.grin@iprosis.com <evgeny.grin@iprosis.com>
2020-11-03 12:07:03 +0200

    updates

--- webcomponent.js
+++ webcomponent.js
@@ -51,6 +51,7 @@
 				table.mobile td::before {
 					content: attr(label);
 					font-weight: bold;
+					color:  #346187;
 					width: 120px;
 					min-width: 120px;
 				}
@@ -90,15 +91,8 @@
 				super();
 				let shadowRoot = this.attachShadow({ mode: 'open' });
 				shadowRoot.appendChild(tmpl.content.cloneNode(true));
-				this.header = 'id:product:price:date';
-				this.data = [
-					'0:Item 0:100$:10/01/2020',
-					'1:Item 1:200$:11/12/2020',
-					'2:Item 2:2300$:17/11/2010',
-					'3:Item 3:377:15/07/1999',
-					'4:Item 4:778$:18/10/2027',
-					'5:Item 5:5$:17/05/2018'
-				];
+				this.header = '';
+				this.data = [];
 				this.breakingWidht = 500; // Lwss that this width - renderd as mobile. TODO: to be exposed as component propety.
 				this.clickedRowData = '';
 				this.fieldsHiddenOnMobile = [];
